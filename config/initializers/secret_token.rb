0bf4e5de8f1a535bd8b92bc2829df93316b60b81
DanielAraujo13 <daniel.ara.6@gmail.com>
2013-10-25 23:20:30 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ContasCesium::Application.config.secret_key_base = 'e5a96da662acb7ebba34aafc83e7d40cc60e03c19edaad3f24f6b23babfcb10f06afbcf3c5019554eeb0a7570046ba92482856d14fdbb8aef5e6aaa430ad6415'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ContasCesium::Application.config.session_store :cookie_store, key: '_contas-CeSIUM_session'
