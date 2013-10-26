2dfeba31f0ac85505cdad3ee68a1ddb5faa0e726
Nuno Carvalho <ntfc30@gmail.com>
2013-10-26 02:52:53 +0100

    Added resources to routes. Removed useless html files

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 ContasCesium::Application.routes.draw do
-  get "category/index"
-  get "category/new"
-  post "category/new" => "category#create"
-  get "category/update"
-  get "category/delete"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   root 'transaction#index'
+  resources :category
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
