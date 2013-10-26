8c7333c6661b7656d1695a01a039d0d7030b118a
Nuno Carvalho <ntfc30@gmail.com>
2013-10-26 03:53:43 +0100

    Added redirect_to category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -10,7 +10,7 @@ class CategoriesController < ApplicationController
   def create
     @category = Category.new(category_params)
     if @category.save
-      render @category
+      redirect_to @category
     else
       render 'new'
     end
